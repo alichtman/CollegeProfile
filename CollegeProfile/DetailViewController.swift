860098e4ea1125998b3aeec2a7f1ec4c775a3338
Aaron Lichtman <AaronLichtman@aarons-air-2>
2016-07-26 03:14:36 -0500

    Created Detail View Controller

---
+++ CollegeProfile/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  CollegeProfile
+//
+//  Created by Aaron Lichtman on 7/26/16.
+//  Copyright © 2016 Aaron Lichtman. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var collegeTextField: UITextField!
+    
+    @IBOutlet weak var stateTextField: UITextField!
+
+    @IBOutlet weak var populationTextField: UITextField!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    //------------------------------------------------------
+    //There is an ! needed on this line.
+    //It makes the compiler happy but I don't understand why?
+    //https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/AutomaticReferenceCounting.html#//apple_ref/doc/uid/TP40014097-CH20-XID_98
+    //This should answer my question, but I haven't read it yet.
+    //------------------------------------------------------
+    var college: College!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collegeTextField.text = college.name
+        stateTextField.text = college.state
+        populationTextField.text = String(college.population)
+        imageView.image = college.image
+
+    }
+    
+    @IBAction func onPressedSaveButton(sender: AnyObject) {
+        
+        college.name = collegeTextField.text!
+        college.state = stateTextField.text!
+        college.population = Int(populationTextField.text!)!
+    }
+
+}
