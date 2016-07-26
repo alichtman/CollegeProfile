e0429633ca8f53a43bc860e2c856413ca9681ced
Aaron Lichtman <AaronLichtman@aarons-air-2>
2016-07-26 00:54:39 -0500

    Connected storyboard and added protocols

--- CollegeProfile/ViewController.swift
+++ CollegeProfile/ViewController.swift
@@ -8,17 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tabelView: UITableView!
+    
+    var colleges = ["Princeton", "UCLA", "UIC", "Harvard"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
